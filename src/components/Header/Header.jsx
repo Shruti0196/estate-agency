41b2d5bbc7fb3a10a5cf29cac9fcff0e16f7ad35
Shruti Prasad <shrutiap2003@gmail.com>
2025-04-02 16:37:10 +0530

    Made Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css'
+
+
+const Header = () => {
+    const headerList = ["Buy", "Rent", "Favorites", "Contact Us"];
+    return (
+        <>
+            <div className="header">
+                <img src="estate-agency-logo.svg" className="logo"></img>
+                <div className="headerList">
+                    {headerList.map((headerData) => (
+                        <span key={headerData}> {headerData} </span>))
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
