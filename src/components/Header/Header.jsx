91ce7c2541fef079ef7c7315e1bff747582df540
Shruti Prasad <shrutiap2003@gmail.com>
2025-04-02 18:18:57 +0530

    Code cleanup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Header.css'
 
 
-const Header = () => {
+function Header() {
     const headerList = ["Buy", "Rent", "Favorites", "Contact Us"];
     return (
         <>
