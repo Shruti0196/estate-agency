3e7485cfeca63c7d4a10213bd3a0e563dd476a87
Shruti Prasad <shrutiap2003@gmail.com>
2025-04-08 16:55:06 +0530

    Consumed Reusable house list component on home page

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReusableHouseListCardView from '../ReusableHouseListCardView/ReusableHouseListCardView'
+
+function HomePage() {
+    return (
+        <ReusableHouseListCardView pageType="houses"></ReusableHouseListCardView>
+    )
+}
+
+export default HomePage;
